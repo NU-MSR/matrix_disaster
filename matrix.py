36c44db51d5245488eab2fa68cff23aeee8dd55f
Matthew Elwin <elwin@u.northwestern.edu>
2019-05-01 10:04:32 -0500

    Lesson 6: Added a setUp() call for a test fixturte
    setUp() is called once before every test.  This enables you
    to easily have common setup code for each test but
    the code is starting from its initial state each time.

--- matrix.py
+++ matrix.py
@@ -90,6 +90,12 @@ class Matrix:
 # methods in this class that start with "test" will automatically be detected
 # By the testing framework
 class TestMatrix(unittest.TestCase):
+    # code in setup is run before every test case
+    def setUp(self):
+        self.mat22 = Matrix(2,2)
+        self.mat33 = Matrix(3,3)
+        self.mat12 = Matrix(1,2)
+        
     # test that a matrix is initialized to all zeros
     # Note: stylistically it is a good idea to make the names for
     # test cases verbose as these are what you see when the test fails
@@ -97,12 +103,12 @@ class TestMatrix(unittest.TestCase):
     def test_matrix_initted_to_zeros(self):
         # assertEqual is an assertion function that verifies that its
         # two arguments are equal to each other
-        self.assertEqual(Matrix(2,2)[(1, 1)], 0)
+        self.assertEqual(self.mat22[(1, 1)], 0)
 
     # test that multiplying incompatible matrix dimensions raises an exception
     def test_matrix_mult_incompatible_dimensions_exception(self):
         with self.assertRaises(Exception):
-            Matrix(3,3)*Matrix(1,2)
+            self.mat33 * self.mat12
 
 # When we run this code as a program, the unittest test runner will execute
 # Try running this code and see what happens.   Note that no tests will run
